311f9474add4117e9e589a0307b822d125fef3ad
sarbjit87 <sarbjit@Sarbjits-MacBook-Air.local>
2017-04-05 20:04:27 -0400

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pyoauth',
+      version='0.1',
+      description='OAuth implementation in Python',
+      url='http://github.com/storborg/funniest',
+      author='Sarbjit Singh',
+      author_email='sarbjit1987@gmail.com',
+      license='MIT',
+      packages=['pyoauth'],
+      zip_safe=False)
